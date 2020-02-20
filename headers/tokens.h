c88ee05f4aeb15b73eea9756e486906553a40a3c
Luc de wit <luc.dewit@hotmail.nl>
2020-02-21 00:24:26 +0100

    tokenizer completely working

--- headers/tokens.h
+++ headers/tokens.h
@@ -5,11 +5,37 @@
 enum Tokentype{
     alph_command,
     alph_string,
-    alph_integer,
+    alph_number,
     alph_variable,
     alph_marker,
     alph_unkown
 };
+
+bool isCommand(const std::string str){
+    if (str=="goto"||str=="let"||str=="more"||str=="less"||str=="print"||str=="get"||str=="exit"||str=="debug"){
+        return true;
+    }
+    return false;
+}
+
+bool isNumber(std::string str){
+    for (int i = 0; i < str.length(); i++){
+        switch(str[i]){
+            case '0':continue;
+            case '1':continue;
+            case '2':continue;
+            case '3':continue;
+            case '4':continue;
+            case '5':continue;
+            case '6':continue;
+            case '7':continue;
+            case '8':continue;
+            case '9':continue;
+            default:return false;
+        }
+    }
+    return true;
+}
 
 class Token{
 private:
@@ -20,7 +46,7 @@ private:
         switch(type){
             case alph_command: return "command";
             case alph_string: return "string";
-            case alph_integer: return "integer";
+            case alph_number: return "number";
             case alph_variable: return "variable";
             case alph_marker: return "marker";
         }
@@ -74,9 +100,11 @@ std::vector<Token> lexer(std::string code){
 
         if (readingString == 0 && !readingComment){
             if (currentChar == '\''){
+                currentToken.setType(alph_string);
                 readingString = 1;
                 continue;
             }else if (currentChar == '"'){
+                currentToken.setType(alph_string);
                 readingString = 2;
                 continue;
             }else if (currentChar == '#'){
@@ -86,7 +114,18 @@ std::vector<Token> lexer(std::string code){
 
             if (currentChar == ' ' || currentChar == '\n'){
                 if (currentToken.getValue() != ""){
-                    //std::cout << currentToken.getValue() << "\n";
+                    if (currentToken.getType() == alph_unkown){
+                        if (isCommand(currentToken.getValue())){
+                            currentToken.setType(alph_command);
+                        }else if(currentToken.getValue()[0] == ':'){
+                            currentToken.setType(alph_marker);
+                            currentToken.setValue(currentToken.getValue().erase(0, 1));
+                        }else if(isNumber(currentToken.getValue())){
+                            currentToken.setType(alph_number);
+                        }else{
+                            currentToken.setType(alph_variable);
+                        }
+                    }
                     tokens.push_back(currentToken.clone());
                     currentToken.reset();
                 }
@@ -112,7 +151,9 @@ std::vector<Token> lexer(std::string code){
         }
     }
 
-    std::cout << currentToken.getValue() << "\n";
+    if (currentToken.getValue() != ""){
+        tokens.push_back(currentToken);
+    }
 
     return tokens;
 }
