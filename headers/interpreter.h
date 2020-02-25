734fb6cbe1983230b699adbfaf742bf9537482d8
Luc de wit <luc.dewit@hotmail.nl>
2020-02-25 01:22:39 +0100

    end of day

--- headers/interpreter.h
+++ headers/interpreter.h
@@ -1,19 +1,27 @@
 #ifndef INTERPRETER_GUARD
 #define INTERPRETER_GUARD 1
 
+#define ARGUMENTS std::vector<Token> arguments
+
 #include <functional>
 #include <iostream>
 #include <vector>
 #include <map>
 
+#include "varsys.h"
 #include "tokens.h"
+
 namespace alphCMDs{
-    void print(std::vector<Token> arguments){
+    void print(ARGUMENTS){
         std::cout << arguments[0].getValue() << "\n";
     }
 
-    void exit(std::vector<Token> arguments){
+    void exit(ARGUMENTS){
         std::exit(std::atoi(arguments[0].getValue().c_str()));
+    }
+
+    void let(ARGUMENTS){
+
     }
 }
 
@@ -21,6 +29,7 @@ void interpretCode(std::vector<Token> tokens){
     std::map<std::string, std::function<void(std::vector<Token>)> > alph_commands;
     alph_commands["print"] = alphCMDs::print;
     alph_commands["exit"] = alphCMDs::exit;
+    alph_commands["let"] = alphCMDs::let;
 
     int tokenIndex = 0;
 
