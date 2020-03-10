b43de662810097f3a2f6f14f9f506d813b4268ad
Luc de wit <luc.dewit@hotmail.nl>
2020-03-10 10:25:36 +0100

    created dlls

---
+++ dll_commands/exit.cpp
@@ -0,0 +1,15 @@
+#define ARGUMENTS std::vector<Token> arguments, std::map<std::string, Variable>& memory, int lineNr, int& currLine, std::map<std::string, int> markers, std::vector<Token> tokens
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+#include "../headers/alphaTools.h"
+#include "../headers/tokens.h"
+#include "../headers/varsys.h"
+
+namespace alphCMDs{
+    void exit(ARGUMENTS){
+        std::exit(std::atoi(arguments[0].getValue().c_str()));
+    }
+}
